2f7c9549dfc79ce641a4218aa745dcf9e37e53aa
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2025-01-11 00:25:46 +0600

    refactor routing in App component; update AppNav to use NavLink for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List of cities</p>} />
             <Route path="cities" element={<p>List of cities</p>} />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
