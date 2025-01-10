df6aa559c2d6696b846da4a6f33e4f48e7ba2ecb
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2025-01-10 17:03:47 +0600

    replace anchor tag with Link component for Pricing navigation

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Homepage() {
   return (
     <div>
       <h1>Homepage</h1>
-      {/* whole page will be reloaded */}
-      <a href="/pricing">Pricing</a>
+
+      {/* Add a link to the Pricing page without reloading the page */}
+      <Link to="/pricing">Pricing</Link>
     </div>
   );
 }
